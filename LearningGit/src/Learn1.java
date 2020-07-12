598a19b8b9f1e1754f90a5f786c0a6d79e97d431
chetan.mali <chetan.mali@visiblealpha.com>
2020-07-12 12:45:03 +0530

    First commit to learn Git

---
+++ LearningGit/src/Learn1.java
@@ -0,0 +1,9 @@
+
+public class Learn1 {
+
+	public static void main(String[] args) {
+		System.out.println("*****************************");
+		System.out.println("Learning how to commit throguh Eclipse IDE to remote repository creted in Github");
+	}
+
+}
