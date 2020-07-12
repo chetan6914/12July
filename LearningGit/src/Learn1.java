9d7f82b7407893e248992601fa083d86d9a53520
chetan.mali <chetan.mali@visiblealpha.com>
2020-07-12 14:33:25 +0530

    Second commit

--- LearningGit/src/Learn1.java
+++ LearningGit/src/Learn1.java
@@ -4,6 +4,8 @@ public class Learn1 {
 	public static void main(String[] args) {
 		System.out.println("*****************************");
 		System.out.println("Learning how to commit throguh Eclipse IDE to remote repository creted in Github");
+		
+		System.out.println("Verify changes through second commit in repository");
 	}
 
 }
